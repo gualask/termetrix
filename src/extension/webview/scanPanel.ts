2847fa103e2cb2100096978ddbd5de70e0562fcc
AndreaMac <gualask.dev@gmail.com>
2025-12-31 16:53:14 +0100

    minor fix

--- src/extension/webview/scanPanel.ts
+++ src/extension/webview/scanPanel.ts
@@ -121,7 +121,7 @@ export class ScanPanel {
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="Content-Security-Policy" content="default-src 'none'; script-src ${webview.cspSource}; style-src ${webview.cspSource} 'unsafe-inline';">
+	<meta http-equiv="Content-Security-Policy" content="default-src 'none'; script-src ${webview.cspSource}; style-src ${webview.cspSource} 'unsafe-inline'; connect-src ${webview.cspSource};">
 	<title>Termetrix Scanner</title>
 	<link rel="stylesheet" href="${styleUri}">
 </head>
